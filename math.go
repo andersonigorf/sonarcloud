63b22f74641a191bc83bc74e58032922273826e7
Anderson Igor <andersonigorf@gmail.com>
2023-03-08 16:41:42 -0300

    first commit

---
+++ math.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(2, 3))
+}
+
+func soma(a int, b int) int {
+	return a + b
+}
+
+// func sub(a int, b int) int {
+// 	return a - b
+// }
+
+// func mult(a int, b int) int {
+// 	return a * b
+// }
+
+// func somaX(a int, b int) int {
+// 	return a + b + a
+// }
+
+// func subX(a int, b int) int {
+// 	return a - b - b
+// }
+
+// func multX(a int, b int) int {
+// 	return a * b * a
+// }
